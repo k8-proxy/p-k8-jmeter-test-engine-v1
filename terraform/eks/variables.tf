084495fb0a10acbd0ec6e7f4f7a2a0188e457b15
Shashank Sinha <sinha.shashank.1989@gmail.com>
2020-11-02 15:23:15 +0400

    add relative path

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -28,13 +28,13 @@ variable cluster_config {
 
 
 variable common_resources {
-  type = string
-  default = "kubectl create serviceaccount --namespace kube-system tiller ;kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller ; sleep 10 ; kubectl patch deploy --namespace kube-system tiller-deploy -p '{\"spec\":{\"template\":{\"spec\":{\"serviceAccount\":\"tiller\"}}}}' ;sleep 10 ;helm init --service-account tiller --upgrade \n"
+  type    = string
+  default = "kubectl create serviceaccount --namespace kube-system tiller ;kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller ; sleep 10 ; sleep 10 ;helm init --service-account tiller --upgrade \n"
 }
 
 variable common_chart {
-  type = string
-  default = "sleep 40 ; helm upgrade --install common --namespace=common ~/Downloads/p-k8-jmeter-test-engine/helm-charts/common-resources/ -f ~/Downloads/p-k8-jmeter-test-engine/helm-charts/common-resources/aws.yaml"
+  type    = string
+  default = "sleep 40 ; helm upgrade --install common --namespace=common  ../../helm-charts/common-resources/ -f ../../helm-charts/common-resources/aws.yaml "
 }
 
 variable cluster_interpreter {
