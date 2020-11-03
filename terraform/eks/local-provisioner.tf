60b48760c49b25e6f7b76788acf146d2bbffe2e2
Shashank Sinha <sinha.shashank.1989@gmail.com>
2020-11-04 03:38:57 +0400

    update local provisioning

--- terraform/eks/local-provisioner.tf
+++ terraform/eks/local-provisioner.tf
@@ -5,7 +5,31 @@ resource "null_resource" "cluster_config" {
   ]
 
   provisioner "local-exec" {
-    command     = var.cluster_config
+    command     = "aws eks --region ${var.region} update-kubeconfig --name ${local.cluster_name}"
+    interpreter = var.cluster_interpreter
+  }
+}
+
+resource "null_resource" "autoscaler_config" {
+  depends_on = [
+    null_resource.cluster_config
+  ]
+
+  provisioner "local-exec" {
+    command     = "sed -i -e 's/CLUSTER_NAME/${local.cluster_name}/g' cluster-autoscaler-autodiscover.yaml"
+    interpreter = var.cluster_interpreter
+  }
+}
+
+
+resource "null_resource" "autoscaler" {
+
+  depends_on = [
+    null_resource.autoscaler_config
+  ]
+
+  provisioner "local-exec" {
+    command     = var.cluster_autoscaler
     interpreter = var.cluster_interpreter
   }
 }
@@ -14,7 +38,7 @@ resource "null_resource" "cluster_config" {
 resource "null_resource" "storage" {
 
   depends_on = [
-    module.eks.workers_asg_arns
+    null_resource.autoscaler
   ]
 
   provisioner "local-exec" {
@@ -24,14 +48,14 @@ resource "null_resource" "storage" {
 }
 
 
-resource "null_resource" "common_config" {
+resource "null_resource" "helm" {
 
   depends_on = [
-    null_resource.cluster_config
+    null_resource.storage
   ]
 
   provisioner "local-exec" {
-    command     = var.common_resources
+    command     = var.helm_permissions
     interpreter = var.cluster_interpreter
   }
 }
@@ -41,7 +65,7 @@ resource "null_resource" "common_config" {
 resource "null_resource" "common" {
 
   depends_on = [
-    null_resource.common_config
+    null_resource.helm
   ]
 
   provisioner "local-exec" {
