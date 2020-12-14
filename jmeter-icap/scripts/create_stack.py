4df14e324b9ff3f437053edd8617bbf5e458fd10
Harut Grigoryan <harut70@hotmail.com>
2020-12-14 07:15:46 +0000

    Upload the file list to minio

--- jmeter-icap/scripts/create_stack.py
+++ jmeter-icap/scripts/create_stack.py
@@ -8,11 +8,14 @@ import subprocess
 import shutil
 import fileinput
 import math
+import boto3
+import requests
+from botocore.client import Config
+from botocore.exceptions import ClientError
 
 logger = logging.getLogger('create_stack')
 
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO').upper()
-
 
 class Main():
 
@@ -42,6 +45,7 @@ class Main():
     enable_tls = False
     tls_verification_method = 'no-verify'
     jmx_file_path = 'none'
+    filelist_bucket = 'filelist'
 
     @staticmethod
     def get_microk8s():
@@ -59,24 +63,24 @@ class Main():
     def verify_url(service_name, url):
         try:
             if not (url.startswith('http://') or url.startswith('https://')):
-                logger.error("{} url must srart with \'http://\' or \'https://\'".format(service_name))
+                print("{} url must srart with \'http://\' or \'https://\'".format(service_name))
                 exit(1)
             port = int(url.split(':', 2)[2])
             if not (port > 0 and port < 0xffff):
-                logger.error("{} url must contain a valid port number".format(service_name))
+                print("{} url must contain a valid port number".format(service_name))
                 exit(1)
         except Exception as e:
-            logger.error("{} URL vertification failed {}".format(service_name, e))
+            print("{} URL vertification failed {}".format(service_name, e))
             exit(1)
 
     @staticmethod
     def verify_file_list(url):
         try:
             if not (url.startswith('http://') or url.startswith('https://')):
-                logger.error("{} file list url must srart with \'http://\' or \'https://\'".format(url))
+                print("{} file list url must srart with \'http://\' or \'https://\'".format(url))
                 exit(1)
         except Exception as e:
-            logger.error("{} URL vertification failed {}".format(service_name, e))
+            print("{} URL vertification failed {}".format(service_name, e))
             exit(1)
 
     @staticmethod
@@ -85,29 +89,29 @@ class Main():
             if not Main.microk8s:
                 subprocess.call(["kubectl", "version"])
         except Exception as e:
-            logger.error("failed to run kubectl: {}".format(e))
+            print("failed to run kubectl: {}".format(e))
             exit(1)
         if int(Main.total_users) <= 0:
-            logger.error("Total users must be positive number")
+            print("Total users must be positive number")
             exit(1)
         if int(Main.users_per_instance) <= 0:
-            logger.error("Users per instance must be positive number")
+            print("Users per instance must be positive number")
             exit(1)
         if int(Main.users_per_instance) > 200:
-            logger.error("Users per instance cannot be greater than 200")
+            print("Users per instance cannot be greater than 200")
             exit(1)
         if int(Main.duration) <= 0:
-            logger.error("Test duration must be positive number")
+            print("Test duration must be positive number")
             exit(1)
         Main.verify_file_list(Main.filelist)
         Main.verify_url('minio', Main.minio_url)
         Main.verify_url('minio external', Main.minio_external_url)
         Main.verify_url('influxdb', Main.influxdb_url)
         if not (int(Main.icap_server_port) > 0 and int(Main.icap_server_port) < 0xffff):
-            logger.error("Wrong icap server port value {}".format(Main.icap_server_port))
+            print("Wrong icap server port value {}".format(Main.icap_server_port))
             exit(1)
         if not os.path.exists(Main.jmx_file_path):
-            logger.error("File {} does not exist".format(Main.jmx_file_path))
+            print("File {} does not exist".format(Main.jmx_file_path))
             exit(1)
 
     @staticmethod
@@ -120,7 +124,7 @@ class Main():
                 os.system("kubectl delete --ignore-not-found jobs -l jobgroup=" + Main.prefix + "-jmeter")
                 os.system("kubectl delete --ignore-not-found secret jmeterconf")
         except Exception as e:
-            logger.error(e)
+            print(e)
             exit(1)
 
     @staticmethod
@@ -131,7 +135,7 @@ class Main():
                     print(line.replace(prev_str, new_str), end='')
             os.remove(filename + '.bak')
         except Exception as e:
-            logger.error(e)
+            print(e)
             exit(1)
 
     @staticmethod
@@ -160,7 +164,7 @@ class Main():
             Main.replace_in_file(jmeter_script_name,"$tls_verification_method$", Main.tls_verification_method)
             return jmeter_script_name
         except Exception as e:
-            logger.error(e)
+            print(e)
             exit(1)
 
     @staticmethod
@@ -191,7 +195,7 @@ class Main():
                 Main.Xmx_value = '2048'
                 return
         except Exception as e:
-            logger.error(e)
+            print(e)
             exit(1)
 
     @staticmethod
@@ -238,7 +242,24 @@ class Main():
             os.remove('job-0.yaml')
 
         except Exception as e:
-            logger.error(e)
+            print(e)
+            exit(1)
+
+    @staticmethod
+    def upload_to_minio(file_path):
+        try:
+            logger.info('Uploading file {}.'.format(file_path))
+            s3 = boto3.resource('s3', endpoint_url=Main.minio_external_url, aws_access_key_id=Main.minio_access_key,
+                                aws_secret_access_key=Main.minio_secret_key, config=Config(signature_version='s3v4'))
+            logger.debug('Checking if the Bucket to upload files exists or not.')
+            if (s3.Bucket(Main.filelist_bucket) in s3.buckets.all()) == False:
+                logger.info('Bucket not Found. Creating Bucket.')
+                s3.create_bucket(Bucket=Main.filelist_bucket)
+            logger.debug('Uploading file to bucket {} minio {}'.format(Main.filelist_bucket, Main.minio_external_url))
+            s3.Bucket(Main.filelist_bucket).upload_file(file_path, 'files')
+            #s3.Bucket(Main.filelist_bucket).download_file('files', 'files')
+        except Exception as e:
+            print("Cannot upload the file list to minio {}".format(e))
             exit(1)
 
     @staticmethod
@@ -323,6 +344,7 @@ class Main():
         print("JMX FILE PATH       {}".format(Main.jmx_file_path))
 
         Main.sanity_checks()
+        Main.upload_to_minio(Main.filelist)
         Main.stop_jmeter_jobs()
         Main.start_jmeter_job()
 
