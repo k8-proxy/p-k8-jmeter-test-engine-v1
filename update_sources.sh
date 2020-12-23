da76ecf4560ec30a32a1e37259f149885f938b76
Harut Grigoryan <harut70@hotmail.com>
2020-12-23 07:24:20 +0000

    Add upstream remote if it is not present

--- update_sources.sh
+++ update_sources.sh
@@ -3,10 +3,11 @@
 repo_url="https://github.com/k8-proxy/p-k8-jmeter-test-engine.git"
 
 if git remote -v | grep upstream | grep $repo_url; then
-    echo "ustream is there"
+    echo "ustream remote is present"
 else
-    echo "ustream is NOT there"
-    exit 1
+    echo "ustream remote is NOT there\nAdding upstream"
+    git remote remove upstream
+    git remote add upstream $repo_url
 fi
 
 git checkout master
