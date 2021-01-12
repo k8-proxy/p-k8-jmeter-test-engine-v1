89c8c4785f2b68bac859b6a0beb0d6cf9869c1bd
Harut Grigoryan <harut70@hotmail.com>
2021-01-12 09:30:59 +0000

    Introduce influxdb metrics management module

---
+++ jmeter-icap/scripts/metrics.py
@@ -0,0 +1,60 @@
+import os
+import logging
+import sys, getopt
+from influxdb import InfluxDBClient
+
+logger = logging.getLogger('proxy-sites')
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO').upper()
+
+class Main():
+
+    hostname = ''
+    hostport = ''
+    jmeter_db_client = InfluxDBClient('', '0', '', '', 'none')
+    icapserver_db_client = InfluxDBClient('', '0', '', '', 'none')
+    proxysite_db_client = InfluxDBClient('', '0', '', '', 'proxysite')
+
+    @staticmethod
+    def verify_database(db):
+        try:
+            db.query('SHOW RETENTION POLICIES;')
+        except Exception as e:
+            print(e)
+            exit(1)
+
+    @staticmethod
+    def log_level(level):
+        logging.basicConfig(level=getattr(logging, level))
+
+    @staticmethod
+    def main(argv):
+        help_string = 'python3 metrics.py -n <host name> -p <host port>'
+        try:
+            opts, args = getopt.getopt(argv,"hn:p:",["name=","port="])
+        except getopt.GetoptError:
+            print (help_string)
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt in ('-h','--help'):
+                print (help_string)
+                sys.exit()
+            elif opt in ('-n', '--name'):
+                Main.hostname = arg
+            elif opt in ('-p', '--port'):
+                Main.hostport = arg
+
+        Main.log_level(LOG_LEVEL)
+        print("host name - {}".format(Main.hostname))
+        print("host port - {}".format(Main.hostport))
+
+        Main.jmeter_db_client = InfluxDBClient(Main.hostname, Main.hostport, '', '', 'jmeter')
+        Main.verify_database(Main.jmeter_db_client)
+
+        Main.icapserver_db_client = InfluxDBClient(Main.hostname, Main.hostport, '', '', 'icapserver')
+        Main.verify_database(Main.icapserver_db_client)
+
+        Main.proxysite_db_client = InfluxDBClient(Main.hostname, Main.hostport, '', '', 'proxysite')
+        Main.verify_database(Main.proxysite_db_client)
+
+if __name__ == "__main__":
+    Main.main(sys.argv[1:])
