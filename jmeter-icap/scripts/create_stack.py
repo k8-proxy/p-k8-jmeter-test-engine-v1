b268e560ebc3a7c38479da041954f355652a3036
Harut Grigoryan <harut70@hotmail.com>
2021-01-18 09:11:29 +0000

    Eliminating outdatd create_stack variables

--- jmeter-icap/scripts/create_stack.py
+++ jmeter-icap/scripts/create_stack.py
@@ -10,10 +10,11 @@ import fileinput
 import math
 import boto3
 import requests
-from botocore.client import Config
+import botocore
 from botocore.exceptions import ClientError
 from ipaddress import ip_address, IPv4Address
 import proxy_sites
+from config_params import Config
 
 logger = logging.getLogger('create_stack')
 
@@ -22,9 +23,9 @@ LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO').upper()
 class Main():
 
     total_users = '100'
-    users_per_instance = '25'
-    duration = '60'
-    filelist = ''
+    #users_per_instance = '25'
+    #duration = '60'
+    #filelist = ''
     minio_url = 'http://minio-service.common:80'
     minio_external_url = 'http://localhost:9000'
     minio_access_key = ''
@@ -44,13 +45,13 @@ class Main():
     parallelism = 1
     microk8s = False
     icap_server_port = '1344'
-    enable_tls = False
-    tls_verification_method = 'no-verify'
+    #enable_tls = False
+    #tls_verification_method = 'no-verify'
     jmx_file_path = 'none'
     filelist_bucket = 'filelist'
     kubectl_string = ''
-    proxy_static_ip = ''
-    load_type = 'Direct'
+    #proxy_static_ip = ''
+    #load_type = 'Direct'
 
     @staticmethod
     def get_microk8s():
@@ -88,48 +89,48 @@ class Main():
         except Exception as e:
             print("ERROR: failed to run kubectl: {}".format(e))
             exit(1)
-        if int(Main.total_users) <= 0:
+        if int(Config.total_users) <= 0:
             print("ERROR: Total users must be positive number")
             exit(1)
-        if int(Main.users_per_instance) <= 0:
+        if int(Config.users_per_instance) <= 0:
             print("ERROR: Users per instance must be positive number")
             exit(1)
-        if int(Main.users_per_instance) > 200:
+        if int(Config.users_per_instance) > 200:
             print("ERROR: Users per instance cannot be greater than 200")
             exit(1)
-        if int(Main.duration) <= 0:
+        if int(Config.duration) <= 0:
             print("ERROR: Test duration must be positive number")
             exit(1)
-        if not os.path.exists(Main.filelist):
-            print("ERROR: File {} does not exist".format(Main.filelist))
+        if not os.path.exists(Config.list):
+            print("ERROR: File {} does not exist".format(Config.list))
             exit(1)
-        Main.verify_url('minio', Main.minio_url)
-        Main.verify_url('minio external', Main.minio_external_url)
-        Main.verify_url('influxdb', Main.influxdb_url)
-        if not (int(Main.icap_server_port) > 0 and int(Main.icap_server_port) < 0xffff):
-            print("ERROR: Wrong icap server port value {}".format(Main.icap_server_port))
+        Main.verify_url('minio', Config.minio_url)
+        Main.verify_url('minio external', Config.minio_external_url)
+        Main.verify_url('influxdb', Config.influxdb_url)
+        if not (int(Config.icap_server_port) > 0 and int(Config.icap_server_port) < 0xffff):
+            print("ERROR: Wrong icap server port value {}".format(Config.icap_server_port))
             exit(1)
-        if not os.path.exists(Main.jmx_file_path):
-            print("ERROR: File {} does not exist".format(Main.jmx_file_path))
+        if not os.path.exists(Config.jmx_file_path):
+            print("ERROR: File {} does not exist".format(Config.jmx_file_path))
             exit(1)
 
         load_type_values = ['Direct','Proxy']
-        if not Main.load_type in load_type_values:
-            print("ERROR: Unsupported load type: {}".format(Main.load_type))
+        if not Config.load_type in load_type_values:
+            print("ERROR: Unsupported load type: {}".format(Config.load_type))
             exit(1)
-        elif Main.load_type == 'Proxy':
+        elif Config.load_type == 'Proxy':
             try: 
-                if not type(ip_address(Main.proxy_static_ip)) is IPv4Address:
-                    print("ERROR: Invalid Proxy IP address {}".format(Main.proxy_static_ip))
+                if not type(ip_address(Config.proxy_static_ip)) is IPv4Address:
+                    print("ERROR: Invalid Proxy IP address {}".format(Config.proxy_static_ip))
                     exit(1)
             except ValueError: 
-                print("ERROR: Invalid Proxy IP address {}".format(Main.proxy_static_ip))
+                print("ERROR: Invalid Proxy IP address {}".format(Config.proxy_static_ip))
                 exit(1)
 
     @staticmethod
     def stop_jmeter_jobs():
         try:
-            os.system(Main.kubectl_string + "-n jmeterjobs delete --ignore-not-found jobs -l jobgroup=" + Main.prefix + "-jmeter")
+            os.system(Main.kubectl_string + "-n jmeterjobs delete --ignore-not-found jobs -l jobgroup=" + Config.prefix + "-jmeter")
             os.system(Main.kubectl_string +" -n jmeterjobs delete --ignore-not-found secret jmeterconf")
         except Exception as e:
             print(e)
@@ -151,25 +152,25 @@ class Main():
         try:
             a = uuid.uuid4()
             jmeter_script_name = str(a)
-            shutil.copyfile(Main.jmx_file_path,jmeter_script_name)
-            Main.replace_in_file(jmeter_script_name,"$number_of_threads$", Main.users_per_instance)
-            Main.replace_in_file(jmeter_script_name,"$duration_in_seconds$", Main.duration)
-            Main.replace_in_file(jmeter_script_name,"$minio_endpoint$", Main.minio_url)
-            Main.replace_in_file(jmeter_script_name,"$minio_access_key$", Main.minio_access_key)
-            Main.replace_in_file(jmeter_script_name,"$minio_secret_key$", Main.minio_secret_key)
-            Main.replace_in_file(jmeter_script_name,"$minio_input_bucket$", Main.minio_input_bucket)
-            Main.replace_in_file(jmeter_script_name,"$minio_output_bucket$", Main.minio_output_bucket)
-            Main.replace_in_file(jmeter_script_name,"$influxdb_url$", Main.influxdb_url)
+            shutil.copyfile(Config.jmx_file_path,jmeter_script_name)
+            Main.replace_in_file(jmeter_script_name,"$number_of_threads$", str(Config.users_per_instance))
+            Main.replace_in_file(jmeter_script_name,"$duration_in_seconds$", str(Config.duration))
+            Main.replace_in_file(jmeter_script_name,"$minio_endpoint$", Config.minio_url)
+            Main.replace_in_file(jmeter_script_name,"$minio_access_key$", Config.minio_access_key)
+            Main.replace_in_file(jmeter_script_name,"$minio_secret_key$", Config.minio_secret_key)
+            Main.replace_in_file(jmeter_script_name,"$minio_input_bucket$", Config.minio_input_bucket)
+            Main.replace_in_file(jmeter_script_name,"$minio_output_bucket$", Config.minio_output_bucket)
+            Main.replace_in_file(jmeter_script_name,"$influxdb_url$", Config.influxdb_url)
             Main.replace_in_file(jmeter_script_name,"$influxHost$", Main.influxHost)
-            Main.replace_in_file(jmeter_script_name,"$prefix$", Main.prefix)
-            Main.replace_in_file(jmeter_script_name,"$icap_server$", Main.icap_server)
-            Main.replace_in_file(jmeter_script_name,"$icap_server_port$", Main.icap_server_port)
+            Main.replace_in_file(jmeter_script_name,"$prefix$", Config.prefix)
+            Main.replace_in_file(jmeter_script_name,"$icap_server$", Config.icap_server)
+            Main.replace_in_file(jmeter_script_name,"$icap_server_port$", Config.icap_server_port)
 
-            if Main.enable_tls:
+            if Config.enable_tls:
                 Main.replace_in_file(jmeter_script_name,"$use_tls$", "true")
             else:
                 Main.replace_in_file(jmeter_script_name,"$use_tls$", "false")
-            Main.replace_in_file(jmeter_script_name,"$tls_verification_method$", Main.tls_verification_method)
+            Main.replace_in_file(jmeter_script_name,"$tls_verification_method$", Config.tls_verification_method)
             return jmeter_script_name
         except Exception as e:
             print(e)
@@ -178,7 +179,7 @@ class Main():
     @staticmethod
     def apply_resource_table():
         try:
-            if int(Main.users_per_instance) <= 50:
+            if int(Config.users_per_instance) <= 50:
                 Main.requests_memory = '768'
                 Main.requests_cpu = '300'
                 Main.limits_memory = '768'
@@ -186,7 +187,7 @@ class Main():
                 Main.Xms_value = '512'
                 Main.Xmx_value = '512'
                 return
-            if int(Main.users_per_instance) <= 100:
+            if int(Config.users_per_instance) <= 100:
                 Main.requests_memory = '1280'
                 Main.requests_cpu = '600'
                 Main.limits_memory = '1280'
@@ -194,7 +195,7 @@ class Main():
                 Main.Xms_value = '1024'
                 Main.Xmx_value = '1024'
                 return
-            if int(Main.users_per_instance) <= 200:
+            if int(Config.users_per_instance) <= 200:
                 Main.requests_memory = '2304'
                 Main.requests_cpu = '1200'
                 Main.limits_memory = '2304'
@@ -221,17 +222,17 @@ class Main():
             if os.path.exists('job-0.yaml'):
                 os.remove('job-0.yaml')
 
-            if Main.load_type == 'Direct':
+            if Config.load_type == 'Direct':
                 shutil.copyfile('jmeter-job-tmpl.yaml','job-0.yaml')
-            elif Main.load_type == 'Proxy':
+            elif Config.load_type == 'Proxy':
                 shutil.copyfile('jmeter-proxy-job-tmpl.yaml','job-0.yaml')
                 Main.replace_in_file('job-0.yaml','$proxy-static-ip$', Main.proxy_static_ip)
-                proxy_sites.Main.file_path = Main.filelist
+                proxy_sites.Main.file_path = Config.list
                 proxy_sites.Main.yaml_file = 'job-0.yaml'
                 proxy_sites.Main.get_domains()
                 proxy_sites.Main.update_yaml()
 
-            Main.parallelism = math.ceil(int(Main.total_users) / int(Main.users_per_instance))
+            Main.parallelism = math.ceil(Config.total_users / Config.users_per_instance)
             print("Number of pods to be created: {}".format(Main.parallelism))
             Main.replace_in_file('job-0.yaml','$parallelism-number', str(Main.parallelism))
 
@@ -258,7 +259,7 @@ class Main():
         try:
             logger.info('Uploading file {}.'.format(file_path))
             s3 = boto3.resource('s3', endpoint_url=Main.minio_external_url, aws_access_key_id=Main.minio_access_key,
-                                aws_secret_access_key=Main.minio_secret_key, config=Config(signature_version='s3v4'))
+                                aws_secret_access_key=Main.minio_secret_key, config=botocore.client.Config(signature_version='s3v4'))
             logger.debug('Checking if the Bucket to upload files exists or not.')
             if (s3.Bucket(Main.filelist_bucket) in s3.buckets.all()) == False:
                 logger.info('Bucket not Found. Creating Bucket.')
@@ -282,14 +283,14 @@ class Main():
             if opt == '-h':
                 print (help_string)
                 sys.exit()
-            elif opt in ("-t", "--total_users"):
-                Main.total_users = arg
-            elif opt in ("-u", "--users_per_instance"):
-                Main.users_per_instance = arg
-            elif opt in ("-d", "--duration"):
-                Main.duration = arg
-            elif opt in ("-l", "--list"):
-                Main.filelist = arg
+            #elif opt in ("-t", "--total_users"):
+            #    Config.total_users = arg
+            #elif opt in ("-u", "--users_per_instance"):
+            #    Config.users_per_instance = arg
+            #elif opt in ("-d", "--duration"):
+            #    Config.duration = arg
+            #elif opt in ("-l", "--list"):
+            #    Config.list = arg
             elif opt in ("-m", "--minio_url"):
                 Main.minio_url = arg
             elif opt in ("-me", "--minio_external_url"):
@@ -310,24 +311,24 @@ class Main():
                 Main.icap_server = arg
             elif opt in ("-port", "--icap_server_port"):
                 Main.icap_server_port = arg
-            elif opt in ("-et", "--enable_tls"):
-                Main.enable_tls = arg
-            elif opt in ("-tls", "--tls_verification_method"):
-                Main.tls_verification_method = arg
-            elif opt in ("-jmx", "--jmx_file_path"):
-                Main.jmx_file_path = arg
-            elif opt in ("-proxy", "--proxy_static_ip"):
-                Main.proxy_static_ip = arg
-            elif opt in ("-load", "--load_type"):
-                Main.load_type = arg
+            #elif opt in ("-et", "--enable_tls"):
+            #    Config.enable_tls = arg
+            #elif opt in ("-tls", "--tls_verification_method"):
+            #    Config.tls_verification_method = arg
+            #elif opt in ("-jmx", "--jmx_file_path"):
+            #    Config.jmx_file_path = arg
+            #elif opt in ("-proxy", "--proxy_static_ip"):
+            #    Config.proxy_static_ip = arg
+            #elif opt in ("-load", "--load_type"):
+            #    Config.load_type = arg
 
         Main.log_level(LOG_LEVEL)
-        print("LOAD TYPE           {}".format(Main.load_type))
+        print("LOAD TYPE           {}".format(Config.load_type))
 
-        print("TOTAL USERS         {}".format(Main.total_users))
-        print("USERS PER INSTANCE  {}".format(Main.users_per_instance))
-        print("TEST DURATION       {}".format(Main.duration))
-        print("FILE LIST           {}".format(Main.filelist))
+        print("TOTAL USERS         {}".format(Config.total_users))
+        print("USERS PER INSTANCE  {}".format(Config.users_per_instance))
+        print("TEST DURATION       {}".format(Config.duration))
+        print("FILE LIST           {}".format(Config.list))
 
 
         print("MINIO URL           {}".format(Main.minio_url))
@@ -347,18 +348,18 @@ class Main():
         print("ICAP SERVER         {}".format(Main.icap_server))
         print("ICAP SERVER PORT    {}".format(Main.icap_server_port))
 
-        print("ENABLE TLS          {}".format(Main.enable_tls))
-        print("TLS VERIFICATION    {}".format(Main.tls_verification_method))
+        print("ENABLE TLS          {}".format(Config.enable_tls))
+        print("TLS VERIFICATION    {}".format(Config.tls_verification_method))
 
 
         Main.get_microk8s()
         print("Micro k8s           {}".format(Main.microk8s))
 
-        print("JMX FILE PATH       {}".format(Main.jmx_file_path))
-        print("PROXY STATIC IP     {}".format(Main.proxy_static_ip))
+        print("JMX FILE PATH       {}".format(Config.jmx_file_path))
+        print("PROXY STATIC IP     {}".format(Config.proxy_static_ip))
 
         Main.sanity_checks()
-        Main.upload_to_minio(Main.filelist)
+        Main.upload_to_minio(Config.list)
         Main.minio_access_key = Main.minio_access_key.replace('&','&amp;')
         Main.minio_secret_key = Main.minio_secret_key.replace('&','&amp;')
         Main.stop_jmeter_jobs()
