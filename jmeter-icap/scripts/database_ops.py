5c97ce26a49022d1d98d1c1a5bbb032b8a792bca
Harut Grigoryan <harut70@hotmail.com>
2021-01-21 11:20:20 +0000

    Utilize ProxySite and Sharepoint queries

--- jmeter-icap/scripts/database_ops.py
+++ jmeter-icap/scripts/database_ops.py
@@ -17,22 +17,57 @@ def database_insert_test(config, run_id, grafana_uid, start_time, final_time):
     InfluxDBMetrics.hostname = Config.influx_host
     InfluxDBMetrics.hostport = Config.influx_port
     InfluxDBMetrics.init()
-    client.write_points([{"measurement": "TestResults", "fields": {
-        "RunId": run_id,
-        "StartTime": start_time,
-        "Duration": config.duration,
-        "GrafanaUid": grafana_uid,
-        "Prefix": config.prefix,
-        "TotalUsers": config.total_users,
-        "LoadType": config.load_type,
-        "EndPointUrl": config.icap_endpoint_url,
-        "TotalRequests": InfluxDBMetrics.total_reguests(config.prefix, start_time, final_time),
-        "SuccessfulRequests": InfluxDBMetrics.successful_reguests(config.prefix, start_time, final_time),
-        "FailedRequests": InfluxDBMetrics.failed_reguests(config.prefix, start_time, final_time),
-        "AverageResponseTime": InfluxDBMetrics.average_resp_time(config.prefix, start_time, final_time),
-        "Status": 0
-    }}])
-
+    if config.load_type == "Direct":
+        client.write_points([{"measurement": "TestResults", "fields": {
+            "RunId": run_id,
+            "StartTime": start_time,
+            "Duration": config.duration,
+            "GrafanaUid": grafana_uid,
+            "Prefix": config.prefix,
+            "TotalUsers": config.total_users,
+            "LoadType": config.load_type,
+            "EndPointUrl": config.icap_endpoint_url,
+            "TotalRequests": InfluxDBMetrics.total_reguests(config.prefix, start_time, final_time),
+            "SuccessfulRequests": InfluxDBMetrics.successful_reguests(config.prefix, start_time, final_time),
+            "FailedRequests": InfluxDBMetrics.failed_reguests(config.prefix, start_time, final_time),
+            "AverageResponseTime": InfluxDBMetrics.average_resp_time(config.prefix, start_time, final_time),
+            "Status": 0
+        }}])
+        return
+    if config.load_type == "Proxy Offline":
+        client.write_points([{"measurement": "TestResults", "fields": {
+            "RunId": run_id,
+            "StartTime": start_time,
+            "Duration": config.duration,
+            "GrafanaUid": grafana_uid,
+            "Prefix": config.prefix,
+            "TotalUsers": config.total_users,
+            "LoadType": config.load_type,
+            "EndPointUrl": config.icap_endpoint_url,
+            "TotalRequests": InfluxDBMetrics.total_reguests_proxysite(config.prefix, start_time, final_time),
+            "SuccessfulRequests": InfluxDBMetrics.successful_reguests_proxysite(config.prefix, start_time, final_time),
+            "FailedRequests": InfluxDBMetrics.failed_reguests_proxysite(config.prefix, start_time, final_time),
+            "AverageResponseTime": InfluxDBMetrics.average_resp_time_proxysite(config.prefix, start_time, final_time),
+            "Status": 0
+        }}])
+        return
+    if config.load_type == "Proxy SharePoint":
+        client.write_points([{"measurement": "TestResults", "fields": {
+            "RunId": run_id,
+            "StartTime": start_time,
+            "Duration": config.duration,
+            "GrafanaUid": grafana_uid,
+            "Prefix": config.prefix,
+            "TotalUsers": config.total_users,
+            "LoadType": config.load_type,
+            "EndPointUrl": config.icap_endpoint_url,
+            "TotalRequests": InfluxDBMetrics.total_reguests_sharepoint(config.prefix, start_time, final_time),
+            "SuccessfulRequests": InfluxDBMetrics.successful_reguests_sharepoint(config.prefix, start_time, final_time),
+            "FailedRequests": InfluxDBMetrics.failed_reguests_sharepoint(config.prefix, start_time, final_time),
+            "AverageResponseTime": InfluxDBMetrics.average_resp_time_sharepoint(config.prefix, start_time, final_time),
+            "Status": 0
+        }}])
+        return
 
 # gets the latest # of rows specified
 def retrieve_test_results(number_of_rows=0):
