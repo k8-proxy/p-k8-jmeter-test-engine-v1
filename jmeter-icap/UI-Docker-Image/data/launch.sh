31ff071c7d2a74e84f570359f38455d70e475965
Harut Grigoryan <harut70@hotmail.com>
2021-02-22 08:29:30 +0000

    Pass KUBECONFIG as env variable to the image

--- jmeter-icap/UI-Docker-Image/data/launch.sh
+++ jmeter-icap/UI-Docker-Image/data/launch.sh
@@ -5,6 +5,4 @@ echo "START Running on `date`"
 rm -f /p-k8-jmeter-test-engine/jmeter-icap/scripts/config.env
 cp /config/config.env /p-k8-jmeter-test-engine/jmeter-icap/scripts
 
-export KUBECONFIG=/config/kube-config
-
 exec /usr/sbin/init
--- jmeter-icap/UI-Docker-Image/startit.sh
+++ jmeter-icap/UI-Docker-Image/startit.sh
@@ -4,4 +4,4 @@ echo "START on `date`"
 
 image_name="ggrig/ui-k8s-jmeter:0.01"
 
-exec sudo docker run -d --name engine_ui --privileged -ti -e container=docker  -v /sys/fs/cgroup:/sys/fs/cgroup -v ${PWD}/config:/config $image_name /usr/sbin/init
+exec sudo docker run -d --name engine_ui --privileged -ti -e container=docker --env KUBECONFIG=/config/kube-config -v /sys/fs/cgroup:/sys/fs/cgroup -v ${PWD}/config:/config $image_name 
