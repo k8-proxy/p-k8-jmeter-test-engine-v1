cf318e405220abcefdfe26f3e6f8db207e5d819f
Harut Grigoryan <harut70@hotmail.com>
2021-02-22 07:16:09 +0000

    Place mounted config.env to the scripts dir

--- jmeter-icap/UI-Docker-Image/data/launch.sh
+++ jmeter-icap/UI-Docker-Image/data/launch.sh
@@ -2,4 +2,7 @@
 
 echo "START Running on `date`"
 
+rm -f /p-k8-jmeter-test-engine/jmeter-icap/scripts/config.env
+cp /config/config.env /p-k8-jmeter-test-engine/jmeter-icap/scripts
+
 exec /usr/sbin/init
