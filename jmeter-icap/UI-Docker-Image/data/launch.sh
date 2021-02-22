68e77833ea672f87eddae40477a215e043c66710
Harut Grigoryan <harut70@hotmail.com>
2021-02-22 07:44:41 +0000

    Pass custom kube-config to the container

--- jmeter-icap/UI-Docker-Image/data/launch.sh
+++ jmeter-icap/UI-Docker-Image/data/launch.sh
@@ -5,4 +5,6 @@ echo "START Running on `date`"
 rm -f /p-k8-jmeter-test-engine/jmeter-icap/scripts/config.env
 cp /config/config.env /p-k8-jmeter-test-engine/jmeter-icap/scripts
 
+export KUBECONFIG=/config/kube-config
+
 exec /usr/sbin/init
